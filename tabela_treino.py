3e1caa0fa009214e3a0d973a234f37a314f47ed5
lstsa <luis.lustosa1981@gmail.com>
2025-06-09 13:47:17 -0300

    codigo(luis)

--- tabela_treino.py
+++ tabela_treino.py
@@ -1,51 +1,76 @@
-from random import sample
+from random import sample, choice
 from time import sleep
 
-def treino(a, b, c):
+def dias_treino(quant_treinos):
     dias_mes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-    dias_treino = sample(dias_mes, a)
+    dias_treino = sample(dias_mes, quant_treinos)
 
+    return dias_treino
+
+def grupos_dia(grupos_treinados, dias):
     grupos_escolhidos = []
-    for g, e in b.items():
+    for g, e in grupos_treinados.items():
         if e == 'S':
             grupos_escolhidos.append(g)
 
-    grupos_dia = {}
-    for dia in dias_treino:
-        grupos_dia[dia] = sample(grupos_escolhidos, 4)
+    grupos_dia_dict = {}
+    for dia in dias:
+        grupos_dia_dict[dia] = sample(grupos_escolhidos, 4)
 
-    tabela = [0] * 12
-    for i in range(len(tabela)):
-        tabela[i] = [0] * 32
+    return grupos_dia_dict
 
-    for i in range(len(tabela)):
-        for j in range(len(tabela[i])):
-            if i == 0 and j == 1:
-                print(f'{'Treinos ˅':^12}', end='')
-
-            elif i == 1 and j == 0:
-                print(f'{'Dias >':^6}', end='')
-
-            elif i == 0 or j ==0:
-                print(f'{'--':^6}', end='')
-
-            elif i == 1 and j == 1:
-                print(f'{'--':^12}', end='')
-
-            elif i == 1:
-                print(f'|{j - 1:^4}|', end='')
-
-            elif j == 1:
-                print(f'{c[i - 2]:^12}', end='')
-
-            elif (j - 2) in dias_treino and c[i - 2] in grupos_dia[j - 2]:
-                print(f'|{'15':^4}|', end='')
-
-            else:
-                print(f'|{'--':^4}|', end='')
-        
-        print()
-
+exercicios_por_grupo = {
+    'Peitoral': [
+        'Supino reto com barra',
+        'Supino inclinado com halteres',
+        'Crossover na polia (ou peck deck)'
+    ],
+    'Costas': [
+        'Puxada na frente na polia alta',
+        'Remada curvada com barra',
+        'Remada unilateral com halteres'
+    ],
+    'Ombros': [
+        'Desenvolvimento com halteres ou barra',
+        'Elevação lateral',
+        'Elevação frontal'
+    ],
+    'Bíceps': [
+        'Rosca direta com barra',
+        'Rosca alternada com halteres',
+        'Rosca no banco Scott'
+    ],
+    'Tríceps': [
+        'Tríceps testa com barra EZ',
+        'Tríceps pulley (cabo) com barra reta ou corda',
+        'Mergulho entre bancos (paralelas ou banco livre)'
+    ],
+    'Glúteos': [
+        'Agachamento livre',
+        'Cadeira abdutora',
+        'Avanço (passada) com halteres'
+    ],
+    'Panturrilha': [
+        'Elevação de panturrilha em pé',
+        'Elevação de panturrilha sentado',
+        'Leg press com ênfase na panturrilha'
+    ],
+    'Posteriores': [
+        'Mesa flexora',
+        'Stiff com barra ou halteres',
+        'Levantamento terra romeno'
+    ],
+    'Quadríceps': [
+        'Agachamento livre',
+        'Leg press',
+        'Cadeira extensora'
+    ],
+    'Abdômen': [
+        'Abdominal na máquina',
+        'Prancha isométrica',
+        'Elevação de pernas na barra fixa ou banco inclinado'
+    ]
+}
 
 print(50 * '-')
 print(f'{'|'}{'A academia':^48}{'|'}')
@@ -97,7 +122,81 @@ for g in grupos_musculares:
 print(50 * '-')
 print()
 
-sleep(1)
-print(f'Esse é o seu treino {nome}:\n')
+dias = dias_treino(quant_treinos)
+treino = grupos_dia(grupos_treinados, dias)
 
-treino(quant_treinos, grupos_treinados, grupos_musculares)
+while True:
+    print(f'{'O que deseja fazer agora?':^50}')
+    print(f'{'[1] para visializar seu treino':^50}')
+    print(f'{'[2] para sugestões de exercícios (por dia)':^50}')
+    print(f'{'[3] visualizar o treino mais longo':^50}')
+    print(f'{'[4] para encerrar programa':^50}')
+    print()
+    resposta = int(input('Digite a opção desejada: '))
+    print()
+
+    while resposta < 1 or resposta > 3:
+        print('Opção inválida! Digite novamente.')
+        resposta = int(input('Digite a opção desejada: '))
+        print()
+    
+    if resposta == 1:
+
+        print(f'{"Visualizando seu treino...":^50}')
+        print(f'{"Informe quanto tempo quer fazer de treino sempre que preciso":^50}')
+        print()
+
+        sleep(2)
+
+        print(f'{"Grupo/Dia":^15}', end='')
+        for dia in range(1, 31): 
+            print(f'{dia:^6}', end='')
+        print()
+
+        for grupo in grupos_musculares:
+            print(f'{grupo:^15}', end='')
+            for dia in range(1, 31):
+                if dia in treino and grupo in treino[dia]:
+                    print(f'{choice():^6}', end='')
+                else:
+                    print(f'{"--":^6}', end='')
+            print()
+        
+        print()
+        print(50 * '-')
+        print()
+            
+    elif resposta == 2:
+        qualDia = int(input('Digite o dia que deseja ver os exercícios: '))
+
+        while qualDia not in dias:
+            print('Dia inválido! Digite novamente.')
+            qualDia = int(input('Digite o dia o qual deseja ver os exercícios: '))
+
+        print(f'{"Lista de sugestões de exercícios para o dia:":^50}')
+
+        sugestao = []
+
+        for grupo in treino[qualDia]:
+            sugestao.append(exercicios_por_grupo[grupo])
+        
+        for i in range(len(sugestao)):
+            for j in range(len(sugestao[i])):
+                print(f'- {sugestao[i][j]}')
+
+        print()
+        print(50 * '-')
+        print()
+
+    elif resposta == 3:
+        print(f'{"Esse é o treino mais longo":^50}')
+        print()
+        sleep(2)
+
+
+
+    elif resposta == 4:
+        print('Encerrando o programa...')
+        sleep(2)
+        print('Programa encerrado com sucesso!')
+        break
